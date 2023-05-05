4c18ee2a5d21ba780293ef03aef29f3404fe6609
Chidera Nwankwo-Igwe <chdrigwe@gmail.com>
2023-05-06 00:14:12 +0100

    styling the education section

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 
+import image from "../images/motion-background.jpg";
+
+const imageAltText = "Just a motion background";
+
 const Education = () => {
     return (
-        <section id="education" className="light">
-        <h1>Education</h1>
-        <div className="container">
+        <section id="education" className="padding">
+        <img className="background" src={image} alt="" />
+        <div style={{ textAlign: "center" }}>
+            <h2>Education</h2>
+        </div>
+        <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem", textAlign: "center" }}>
             <div className="row">
-            <div className="col-12">
-                <h2>University of Nigeria</h2>
-                <h3>Bachelor of Science in Computer Science</h3>
-                <h4>2019 - 2024</h4>
+            <div className="large">
+                <h3>University of Nigeria</h3>
+                <h4>Bachelor of Science in Computer Science</h4>
+                <h5>2019 - 2024</h5>
             </div>
             </div>
         </div>
