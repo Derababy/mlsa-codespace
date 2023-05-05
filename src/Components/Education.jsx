543e54c33961dbdc4019949a9f22bb9f9186e510
Chidera Nwankwo-Igwe <chdrigwe@gmail.com>
2023-05-05 23:24:01 +0100

    trying copilot for the first time

---
+++ src/Components/Education.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Education = () => {
+    return (
+        <section id="education" className="light">
+        <h1>Education</h1>
+        <div className="container">
+            <div className="row">
+            <div className="col-12">
+                <h2>University of Nigeria</h2>
+                <h3>Bachelor of Science in Computer Science</h3>
+                <h4>2019 - 2024</h4>
+            </div>
+            </div>
+        </div>
+        </section>
+    );
+}
+
+export default Education;
