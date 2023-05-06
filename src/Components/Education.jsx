72ff0f58357aef149ff4c877aeaab41ae190324a
Chidera Nwankwo-Igwe <chdrigwe@gmail.com>
2023-05-06 01:00:36 +0100

    still trying to make changes

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -11,13 +11,11 @@ const Education = () => {
         <div style={{ textAlign: "center" }}>
             <h2>Education</h2>
         </div>
-        <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem", textAlign: "center" }}>
-            <div className="row">
+        <div style={{ position: "absolute", top: "10rem", left: "5rem", margin: "auto", width: "17rem", textAlign: "center" }}>
             <div className="large">
                 <h3>University of Nigeria</h3>
                 <h4>Bachelor of Science in Computer Science</h4>
                 <h5>2019 - 2024</h5>
-            </div>
             </div>
         </div>
         </section>
